e4d1162b8710f1a218ed9292c23cc99ba62c071c
Mitchelle Creado <creadomitchelle@gmail.com>
2022-12-30 09:07:44 -0600

    fft changes

--- fft.py
+++ fft.py
@@ -10,7 +10,7 @@ import csv
  
 # Define folder to save uploaded files to process further
 UPLOAD_FOLDER = os.path.join('staticFiles', 'uploads')
- 
+SHOW = False
 # Define allowed files (for this example I want only csv file)
 ALLOWED_EXTENSIONS = {'csv'}
  
@@ -24,7 +24,9 @@ app.secret_key = 'This is your secret key to utilize session in Flask'
  
 @app.route('/')
 def index():
-    return render_template('fft.html')
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'dummy.png')
+    return render_template('fft.html', user_image = full_filename)
+    #  render_template('fft.html')
  
 @app.route('/',  methods=("POST", "GET"))
 def uploadFile():
@@ -34,61 +36,64 @@ def uploadFile():
  
         # Extracting uploaded data file name
         data_filename = secure_filename(uploaded_df.filename)
- 
+        print(data_filename)
+        print(uploaded_df)
         # flask upload file to database (defined uploaded folder in static path)
         uploaded_df.save(os.path.join(app.config['UPLOAD_FOLDER'], data_filename))
  
         # Storing uploaded file path in flask session
         session['uploaded_data_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], data_filename)
+
+        with open(os.path.join('./staticFiles/uploads/sheets.csv'), newline='') as csvfile:
+            list_data = list(csv.reader(csvfile))
+
+            list_to_arr = np.array(list_data)
+
+        squareimpulse = list_to_arr.flatten()
+        # squareimpulse = np.array([0,0,0,0,0,1,1,1,1,1,0,0,0,0,0])
+
+        img = (squareimpulse)
+        f = np.fft.fft(img)
+        fshift = np.fft.fftshift(f)
+        magnitude_spectrum = (np.abs(fshift))
+        # plt.plt.switch_backend('Agg') 
+        plt.switch_backend('agg')
+        plt.subplot(121)
+        plt.plot(img)
+        plt.title('Input Image')
+        plt.xticks([]), plt.yticks([])
+
+        plt.subplot(122)
+        plt.plot(magnitude_spectrum)
+        plt.title('Magnitude Spectrum')
+        plt.xticks([]), plt.yticks([])
+        plt.xlabel('Frequency')
+        plt.ylabel('Magnitude')
+        file_name = os.path.join(app.config['UPLOAD_FOLDER'] + '/my_plot.png')
+        plt.savefig(file_name)
+        # plt.show()
+        # // end
+
+        # Retrieving uploaded file path from session
+        # data_file_path = session.get('uploaded_data_file_path', None)
+    
+        # read csv file in python flask (reading uploaded csv file from uploaded server location)
+        # uploaded_df = pd.read_csv(data_file_path)
+    
+        # pandas dataframe to html table flask
+        # uploaded_df_html = uploaded_df.to_html()
+        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'my_plot.png')
+        print(full_filename)
+        return render_template('fft.html', user_image = full_filename)
+        # return render_template('fft.html')
  
-        return render_template('fft.html')
- 
-@app.route('/fft')
-def showData():
-    with open(os.path.join('./staticFiles/uploads/sheets.csv'), newline='') as csvfile:
-        list_data = list(csv.reader(csvfile))
-
-        list_to_arr = np.array(list_data)
-
-    squareimpulse = list_to_arr.flatten()
-    # squareimpulse = np.array([0,0,0,0,0,1,1,1,1,1,0,0,0,0,0])
-
-    img = (squareimpulse)
-    f = np.fft.fft(img)
-    fshift = np.fft.fftshift(f)
-    magnitude_spectrum = (np.abs(fshift))
-    # plt.plt.switch_backend('Agg') 
-    plt.switch_backend('agg')
-    plt.subplot(121)
-    plt.plot(img)
-    plt.title('Input Image')
-    plt.xticks([]), plt.yticks([])
-
-    plt.subplot(122)
-    plt.plot(magnitude_spectrum)
-    plt.title('Magnitude Spectrum')
-    plt.xticks([]), plt.yticks([])
-    plt.xlabel('Frequency')
-    plt.ylabel('Magnitude')
-    file_name = os.path.join(app.config['UPLOAD_FOLDER'] + '/my_plot.png')
-    plt.savefig(file_name)
-    # plt.show()
-    # // end
-
-    # Retrieving uploaded file path from session
-    # data_file_path = session.get('uploaded_data_file_path', None)
- 
-    # read csv file in python flask (reading uploaded csv file from uploaded server location)
-    # uploaded_df = pd.read_csv(data_file_path)
- 
-    # pandas dataframe to html table flask
-    # uploaded_df_html = uploaded_df.to_html()
-    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'my_plot.png')
-    return render_template('show_csv_data.html', user_image = full_filename)
+# @app.route('/', methods=("PUT"))
+# def showData():
+    
  
 @app.route('/back')
 def back():
     return render_template('fft.html')
 
 if __name__=='__main__':
-    app.run(host='127.0.0.1', port=5007, debug = True)
+    app.run(host='127.0.0.1', port=5008, debug = True)
